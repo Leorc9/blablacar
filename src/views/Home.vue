b8cafdc73a9cbbc6799c58c6b371f6b54a44167f
Leo Degrugillier <leo.degrugillier2205@gmail.com>
2026-02-24 16:35:59 +0100

    feat: Add views and router packages

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>Bienvenue sur BlaBlaCar</h1>
+    <p>Page d'accueil en construction</p>
+  </div>
+</template>
+
+<script setup>
+// Page Home basique
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 50px 20px;
+}
+
+h1 {
+  color: #00aff5;
+  margin-bottom: 20px;
+}
+
+p {
+  color: #666;
+}
+</style>
