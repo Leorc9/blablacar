affab7969838cd8f24b7c753e712c0717ee4e95e
Leo Degrugillier <leo.degrugillier2205@gmail.com>
2026-03-24 20:33:29 +0100

    feat: add authentication guards to protect routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores/user'
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import Register from '@/views/Register.vue'
@@ -9,19 +10,57 @@ import MyRides from '@/views/MyRides.vue'
 import Profile from '@/views/Profile.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: Login },
-  { path: '/register', component: Register },
-  { path: '/create-ride', component: CreateRide },
-  { path: '/search-rides', component: SearchRides },
-  { path: '/ride/:id', component: RideDetail },
-  { path: '/my-rides', component: MyRides },
-  { path: '/profile', component: Profile }
+  { 
+    path: '/', 
+    component: Home 
+  },
+  { 
+    path: '/login', 
+    component: Login 
+  },
+  { 
+    path: '/register', 
+    component: Register 
+  },
+  { 
+    path: '/search-rides', 
+    component: SearchRides 
+  },
+  { 
+    path: '/ride/:id', 
+    component: RideDetail 
+  },
+  { 
+    path: '/create-ride', 
+    component: CreateRide,
+    meta: { requiresAuth: true }
+  },
+  { 
+    path: '/my-rides', 
+    component: MyRides,
+    meta: { requiresAuth: true }
+  },
+  { 
+    path: '/profile', 
+    component: Profile,
+    meta: { requiresAuth: true }
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+// Navigation guard
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore()
+  
+  if (to.meta.requiresAuth && !userStore.currentUser) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
